71396c7377934cd3a12044abdf0d5435533364bb
kirtisel <kirtisel@gmil.com>
2025-02-18 01:16:11 +0300

    nothing new

--- src/main/java/io/project/Bot/config/BotInitializer.java
+++ src/main/java/io/project/Bot/config/BotInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.updates.DeleteWebhook;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -28,6 +29,7 @@ public class BotInitializer {
        // setWebhook.setUrl(config.getUrl()); // записать url
 
             telegramBotsApi.registerBot(bot);
+            //bot.execute(new DeleteWebhook());
 
             System.out.println("Бот успешно зарегистрирован. Вебхук установлен на: {}" + config.getUrl());
         }
