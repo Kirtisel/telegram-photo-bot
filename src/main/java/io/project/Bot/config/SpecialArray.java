5ab0a1fcc8231a52a070c6bc5694fd330b741198
kirtisel <kirtisel@gmil.com>
2025-02-20 12:58:46 +0300

    2 new class SpecialArrayList and SpecialArray

---
+++ src/main/java/io/project/Bot/config/SpecialArray.java
@@ -0,0 +1,84 @@
+package io.project.Bot.config;
+
+
+public class SpecialArray {
+    private int length;
+    private int height;
+    private Object[][] objArray;
+    public Object[] types;
+    private int iter;
+//    A[] A;
+//    B[] B;
+//    C[] C;
+
+
+
+
+    private SpecialArray(int height, Object[] types) {
+        this.length = types.length;
+        this.height = height;
+        this.objArray = new Object[height][types.length];
+        this.types = types;
+        this.iter = 0;
+    }
+
+    public static SpecialArray
+    createSpecialArray(int height, Class<?> ... types){//Class<A>[] aType, Class<B> bType, Class<C> cType) {
+        return new SpecialArray(height, types);
+    }
+
+    public static void main(String[] args) {
+        SpecialArray array = SpecialArray.createSpecialArray(1, String.class, Integer.class, String.class);
+        array.add("a", 1, "");
+        //System.out.println(array.types[0]);
+        //System.out.println(array.types[0].getClass());
+        //System.out.println("a".getClass());
+        System.out.println(array.get(0)[0]);
+    }
+
+//    private void deleteFirstElement(){
+//        for(int i = 0; i < length; i++){
+//            objArray[0][i] = null;
+//        }
+//    }
+
+    private void moveDown(){
+        for(int i = 1; i < height; i++){
+            for (int k = 0; k < length; k++){
+                objArray[i-1][k] = objArray[i][k];
+            }
+        }
+    }
+
+
+    public boolean add (Object ... objects){
+        if (objects.length != length) throw new IllegalArgumentException("Too much or not enough arguments");
+
+        if(iter==height-1){
+            // написать удаление элемента
+            moveDown();
+        }
+
+        for(int i = 0; i < length; i++){
+            if (types[i] != objects[i].getClass()) {
+                throw new IllegalArgumentException("Class to add and class declared in createSpecialArray() are not same");
+            }
+            objArray[iter][i] = objects[i];
+        }
+
+        iter++;
+        return true;
+    }
+
+    public Object[] get (int index){
+        if (index < 0 || index > length-1) throw new IndexOutOfBoundsException();
+
+        Object[] arr = new Object[length];
+     for (int i = 0; i < length; i++) {
+         arr[i] = objArray[index][i];
+     }
+
+     return arr;
+    }
+
+}
---
+++ src/main/java/io/project/Bot/config/SpecialList.java
@@ -0,0 +1,41 @@
+package io.project.Bot.config;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
+
+public class SpecialList<K, V> extends ArrayList<Map.Entry<K, V>>{
+    ArrayList<Map.Entry<K, V>> list = new ArrayList<>();
+    private int capacity;
+
+    private SpecialList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public static <K, V> SpecialList<K, V> createSpecialList(int capacity, Class<K> keyType, Class<V> valueType) {
+        return new SpecialList<>(capacity);
+    }
+
+    @Override
+    public boolean add(Map.Entry<K, V> entry) {
+        if (size()==capacity) remove(0);
+        super.add(entry);
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        SpecialList specialList =
+                SpecialList.createSpecialList(2, String.class, String.class);
+        for(int i = 0; i<30; i++)
+        specialList.getRandom();
+    }
+
+    public Map.Entry<K, V> getRandom(){
+        Random rn = new Random();
+        int randomNum = rn.nextInt(capacity);
+        System.out.println(randomNum);
+        return null;// get(randomNum);
+    }
+}
