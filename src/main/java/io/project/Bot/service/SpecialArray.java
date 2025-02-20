fed7f70c490765682d6ec26df7c9ed63b6ee5ef9
kirtisel <kirtisel@gmil.com>
2025-02-20 15:55:03 +0300

    reviewed SpecialArray

--- src/main/java/io/project/Bot/service/SpecialArray.java
+++ src/main/java/io/project/Bot/service/SpecialArray.java
@@ -3,18 +3,19 @@ package io.project.Bot.service;
 import java.util.Random;
 
 public class SpecialArray {
-    private int length;
-    private int height;
-    private Object[][] objArray;
-    private Object[] types;
-    private int iter;
+    private final int length;
+    private final int height;
+    private final Object[][] objArray;
+    private final Class<?>[] types;
+    private int currentIndex;
+    private final Random random = new Random();
 
-    private SpecialArray(int height, Object[] types) {
+    private SpecialArray(int height, Class<?>[] types) {
         this.length = types.length;
         this.height = height;
         this.objArray = new Object[height][types.length];
         this.types = types;
-        this.iter = 0;
+        this.currentIndex = 0;
     }
 
     public static SpecialArray
@@ -49,48 +50,47 @@ public class SpecialArray {
     }
 
     private void moveDown(){
-        for(int i = 1; i < height; i++){
-            for (int k = 0; k < length; k++){
-                objArray[i-1][k] = objArray[i][k];
-            }
+//        for(int i = 1; i < height; i++){
+//            for (int k = 0; k < length; k++){
+//                objArray[i-1][k] = objArray[i][k];
+        if (height > 1) {
+            System.arraycopy(objArray, 1, objArray, 0, height - 1);
         }
+
+
     }
 
 
     public boolean add (Object ... objects){
         if (objects.length != length) throw new IllegalArgumentException("Too much or not enough arguments");
 
-        if(iter==height){
-            // написать удаление элемента
-            iter--;
-            moveDown();
+        if (currentIndex == height) {
+            if (height == 1) {
+                currentIndex = 0; // Сбрасываем индекс, если высота равна 1
+            } else {
+                currentIndex--;
+                moveDown();
+            }
         }
 
         for(int i = 0; i < length; i++){
-            if (types[i] != objects[i].getClass()) {
+            if (types[i].isInstance(objects[i])) {
                 throw new IllegalArgumentException("Class to add and class declared in createSpecialArray() are not same");
             }
-            objArray[iter][i] = objects[i];
+            objArray[currentIndex][i] = objects[i];
         }
 
-        iter++;
+        currentIndex++;
         return true;
     }
 
     public Object[] get (int index){
-        if (index < 0 || index > height-1) throw new IndexOutOfBoundsException();
-
-        Object[] arr = new Object[length];
-     for (int i = 0; i < length; i++) {
-         arr[i] = objArray[index][i];
-     }
-
-     return arr;
+        if (index < 0 || index >= height) throw new IndexOutOfBoundsException();
+     return objArray[index];
     }
 
     public Object[] getRandom(){
-        Random rn = new Random();
-        int randomNum = rn.nextInt(height);
+        int randomNum = random.nextInt(height);
         return get(randomNum);// get(randomNum);
     }
 
